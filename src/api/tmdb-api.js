45a211d66875eb243a598d468bb3ab4d8ce3a72e
Cathal O'Connor <catoconn@redhat.com>
2023-05-09 22:11:38 +0100

    refactor endpoints

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -1,31 +1,35 @@
 export const getMovies = () => {
     return fetch(
-        //`https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
         `/api/movies`, {
-            headers: {
-                'Authorization': window.localStorage.getItem('token')
-        }}
+        headers: {
+            'Authorization': window.localStorage.getItem('token')
+        }
+    }
     )
         .then(res => res.json())
         .then(json => json.results);
 };
 
-export const getMovie = (args) => {
-    const [, idPart] = args.queryKey;
-    const { id } = idPart;
+export const getMovie = (id) => {
     return fetch(
         `/api/movies/${id}`, {
-            headers: {
-                'Authorization': window.localStorage.getItem('token')
-        }}
+        headers: {
+            'Authorization': window.localStorage.getItem('token')
+        }
+    }
     ).then(res => res.json());
 };
 
 export const getGenres = () => {
     return fetch(
-        "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
-        import.meta.env.VITE_TMDB_KEY +
-        "&language=en-US"
+        //"https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+        //import.meta.env.VITE_TMDB_KEY +
+        //"&language=en-US"
+        `/api/genres/`, {
+        headers: {
+            'Authorization': window.localStorage.getItem('token')
+        }
+    }
     )
         .then(res => res.json())
         .then(json => json.genres);
@@ -33,16 +37,22 @@ export const getGenres = () => {
 
 export const getMovieImages = (id) => {
     return fetch(
-        `https://api.themoviedb.org/3/movie/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
+        `/api/movies/${id}/images`, {
+        headers: {
+            'Authorization': window.localStorage.getItem('token')
+        }
+    })
         .then((res) => res.json())
         .then((json) => json.posters);
 };
 
 export const getMovieCredits = (id) => {
     return fetch(
-        `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
+        `/api/movies/${id}/credits`, {
+        headers: {
+            'Authorization': window.localStorage.getItem('token')
+        }
+    })
         .then((res) => res.json())
         .then(json => json.cast);
 
@@ -50,8 +60,11 @@ export const getMovieCredits = (id) => {
 
 export const getSimilarMovies = (id) => {
     return fetch(
-        `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
+        `/api/movies/${id}/similar`, {
+        headers: {
+            'Authorization': window.localStorage.getItem('token')
+        }
+    })
         .then((res) => res.json())
         .then(json => json.results);
 
@@ -59,7 +72,11 @@ export const getSimilarMovies = (id) => {
 
 export const getTVShows = () => {
     return fetch(
-        `https://api.themoviedb.org/3/discover/tv?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+        `/api/tvshows`, {
+        headers: {
+            'Authorization': window.localStorage.getItem('token')
+        }
+    }
     )
         .then(res => res.json())
         .then(json => json.results);
@@ -67,23 +84,32 @@ export const getTVShows = () => {
 
 export const getTVShow = (id) => {
     return fetch(
-        `https://api.themoviedb.org/3/tv/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
+        `/api/tvshows/${id}`, {
+        headers: {
+            'Authorization': window.localStorage.getItem('token')
+        }
+    })
         .then(res => res.json());
 };
 
 export const getTVShowImages = (id) => {
     return fetch(
-        `https://api.themoviedb.org/3/tv/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
+        `/api/tvshows/${id}/images`, {
+        headers: {
+            'Authorization': window.localStorage.getItem('token')
+        }
+    })
         .then((res) => res.json())
         .then((json) => json.posters);
 };
 
 export const getTVShowCredits = (id) => {
     return fetch(
-        `https://api.themoviedb.org/3/tv/${id}/credits?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
+        `/api/tvshows/${id}/credits`, {
+        headers: {
+            'Authorization': window.localStorage.getItem('token')
+        }
+    })
         .then((res) => res.json())
         .then(json => json.cast);
 
@@ -91,24 +117,33 @@ export const getTVShowCredits = (id) => {
 
 export const getPerson = (id) => {
     return fetch(
-        `https://api.themoviedb.org/3/person/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
+        `/api/person/${id}`, {
+        headers: {
+            'Authorization': window.localStorage.getItem('token')
+        }
+    })
         .then((res) => res.json());
 
 }
 
 export const getPersonImages = (id) => {
     return fetch(
-        `https://api.themoviedb.org/3/person/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
+        `/api/person/${id}/images`, {
+        headers: {
+            'Authorization': window.localStorage.getItem('token')
+        }
+    })
         .then((res) => res.json())
         .then((json) => json.profiles);
 };
 
 export const getSimilarTVShows = (id) => {
     return fetch(
-        `https://api.themoviedb.org/3/tv/${id}/similar?api_key=${import.meta.env.VITE_TMDB_KEY}`
-    )
+        `/api/tvshows/${id}/similar`, {
+        headers: {
+            'Authorization': window.localStorage.getItem('token')
+        }
+    })
         .then((res) => res.json())
         .then(json => json.results);
 
@@ -116,7 +151,7 @@ export const getSimilarTVShows = (id) => {
 
 export const getMovieReviews = (id) => {
     return fetch(
-        `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${import.meta.env.VITE_TMDB_KEY}`
+        //`https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${import.meta.env.VITE_TMDB_KEY}`
     )
         .then((res) => res.json())
         .then((json) => {
@@ -126,7 +161,7 @@ export const getMovieReviews = (id) => {
 
 export const getTVShowReviews = (id) => {
     return fetch(
-        `https://api.themoviedb.org/3/tv/${id}/reviews?api_key=${import.meta.env.VITE_TMDB_KEY}`
+        //`https://api.themoviedb.org/3/tv/${id}/reviews?api_key=${import.meta.env.VITE_TMDB_KEY}`
     )
         .then((res) => res.json())
         .then((json) => {
@@ -136,7 +171,7 @@ export const getTVShowReviews = (id) => {
 
 export const getUpcomingMovies = () => {
     return fetch(
-        `https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+        //`https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
     )
         .then(res => res.json())
         .then(json => json.results);
@@ -144,7 +179,7 @@ export const getUpcomingMovies = () => {
 
 export const getPopularTVShows = () => {
     return fetch(
-        `https://api.themoviedb.org/3/tv/popular?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+        //`https://api.themoviedb.org/3/tv/popular?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
     )
         .then(res => res.json())
         .then(json => json.results);
